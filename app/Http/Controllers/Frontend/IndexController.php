23cda046f4922374b066c74c5f393eee3322a8fc
Md. Abu Bakkar Siddik <abubakkarsiddik.cse@gmail.com>
2022-03-22 13:38:05 +0600

    new product

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Slider;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -15,8 +16,9 @@ class IndexController extends Controller
     public function index()
     {
         $sliders = Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
+        $products = Product::where('status', 1)->orderBy('id', 'DESC')->limit(6)->get();
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.index', compact('categories', 'sliders'));
+        return view('frontend.index', compact('categories', 'sliders', 'products'));
     }
 
     public function userLogout()
